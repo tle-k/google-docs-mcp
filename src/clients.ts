d9c8a6d8296c6fca455c7daaf88738c1d4778e27
tle-k <58928248+tle-k@users.noreply.github.com>
2026-05-05 00:41:48 -0400

    Refactor Google client initialization and add new clients

--- src/clients.ts
+++ src/clients.ts
@@ -1,21 +1,48 @@
 import { google } from 'googleapis';
 import * as fs from 'fs';
 
-export let docsClient: any;
-export let driveClient: any;
+let docsClient: any;
+let driveClient: any;
+let sheetsClient: any;
+let gmailClient: any;
+let calendarClient: any;
+let authClient: any;
+let scriptClient: any;
 
 export async function initializeGoogleClient() {
   const saJson = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
-  if (!saJson) throw new Error("Missing GOOGLE_SERVICE_ACCOUNT_JSON");
+  if (!saJson) {
+    console.error("FATAL: GOOGLE_SERVICE_ACCOUNT_JSON missing!");
+    process.exit(1);
+  }
 
   fs.writeFileSync('/tmp/sa.json', saJson);
   
   const auth = new google.auth.GoogleAuth({
     keyFile: '/tmp/sa.json',
-    scopes: ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/documents']
+    scopes: [
+      'https://www.googleapis.com/auth/documents',
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/spreadsheets',
+      'https://www.googleapis.com/auth/gmail.modify',
+      'https://www.googleapis.com/auth/calendar.events',
+      'https://www.googleapis.com/auth/script.projects'
+    ]
   });
 
-  const authClient = await auth.getClient();
-  docsClient = google.docs({ version: 'v1', auth: authClient });
-  driveClient = google.drive({ version: 'v3', auth: authClient });
+  authClient = await auth.getClient();
+  docsClient = google.docs({ version: 'v1', auth: authClient as any });
+  driveClient = google.drive({ version: 'v3', auth: authClient as any });
+  sheetsClient = google.sheets({ version: 'v4', auth: authClient as any });
+  gmailClient = google.gmail({ version: 'v1', auth: authClient as any });
+  calendarClient = google.calendar({ version: 'v3', auth: authClient as any });
+  scriptClient = google.script({ version: 'v1', auth: authClient as any });
 }
+
+export const getDocsClient = () => docsClient;
+export const getDriveClient = () => driveClient;
+export const getSheetsClient = () => sheetsClient;
+export const getGmailClient = () => gmailClient;
+export const getCalendarClient = () => calendarClient;
+export const getAuthClient = () => authClient;
+export const getScriptClient = () => scriptClient;
